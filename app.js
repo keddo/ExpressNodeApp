a976ae9a7c9f654640265de52b9d221b8a0d9973
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-02 12:12:50 +0300

    Add packages debug, morgan and chalk for debugging

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
 dotenv.config();
 
 const app = express();
+
+
+app.use(morgan('combined'));
+
 app.get('/', function (req, res) {
     res.send({ message: 'hello', tech: 'express' });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, function () {
-    console.log(`Server running at: https://localhost:${PORT}`);
+    debug(`Server running at: https://localhost:${chalk.green(PORT)}`);
 });
