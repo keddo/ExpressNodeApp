242b97bb8bc859d2230c48032bf130b6d0149d29
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-09 19:02:25 +0300

    Display book list from hardcoded arrays

--- app.js
+++ app.js
@@ -16,9 +16,68 @@ app.use(express.static(path.join(__dirname, '/public/')));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-const route = express.Router();
+const bookRouter = express.Router();
 
-route.route('/').get(function (req, res) {
+const books = [
+    {
+        title: "War and Peace",
+        genre: "Historical Fiction",
+        author: 'Lev Nikolayevich Tolstoy',
+        image_format: 'png',
+        read: false
+    },
+    {
+        title: "les Miserables",
+        genre: "Historical Fiction",
+        author: 'Victor Hugo',
+        image_format: 'png',
+        read: false
+    },
+    {
+        title: "A Journey into the center of the Earth.",
+        genre: "Science Fiction",
+        author: 'Jules Verne',
+        image_format: 'png',
+        read: false
+    },
+    {
+        title: "The Dark World",
+        genre: "Fantasy",
+        author: 'Henry Kuttner',
+        image_format: 'png',
+        read: false
+    },
+    {
+        title: "The Time Machine",
+        genre: "Science Fiction",
+        author: 'H. G. Wells',
+        image_format: 'png',
+        read: false
+    },
+    {
+        title: "The Wind in the Willows",
+        genre: "Fantasy",
+        author: 'Kenneth Grahame',
+        image_format: 'jpg',
+        read: false
+    },
+    {
+        title: "Life on the Mississippi",
+        genre: "History",
+        author: 'Kenneth Grahame',
+        image_format: 'png',
+        read: false
+    },
+    {
+        title: "Life on the Mississippi",
+        genre: "History",
+        author: 'Kenneth Grahame',
+        image_format: 'png',
+        read: false
+    }
+];
+
+bookRouter.route('/').get(function (req, res) {
     // eslint-disable-next-line no-undef
     res.render('index',
         {
@@ -26,10 +85,10 @@ route.route('/').get(function (req, res) {
             { link: '/api/books', title: "Books" },
             { link: '/api/authors', title: "Authors" },
             { link: '/api/category', title: "Category" }],
-            title: 'My App'
+            title: 'Bookstore'
         });
 });
-route.route('/books').get(function (req, res) {
+bookRouter.route('/books').get(function (req, res) {
     // eslint-disable-next-line no-undef
     res.render('book_list_view',
         {
@@ -37,11 +96,12 @@ route.route('/books').get(function (req, res) {
             { link: '/api/books', title: "Books" },
             { link: '/api/authors', title: "Authors" },
             { link: '/api/categories', title: "Category" }],
-            title: 'My App'
+            title: 'Bookstore',
+            books
         });
 });
 
-app.use('/api', route);
+app.use('/api', bookRouter);
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3002;
 
