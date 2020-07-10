549a029901078648cd984388329314994e5743d7
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-10 13:22:49 +0300

    Retrieve single book from database

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,67 +1,9 @@
 const express = require('express');
 const bookRouter = express.Router();
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 const debug = require('debug')('app:bookRoutes');
 
 function router(nav) {
-    // const books = [
-    //     {
-    //         title: "War and Peace",
-    //         genre: "Historical Fiction",
-    //         author: 'Lev Nikolayevich Tolstoy',
-    //         image_format: 'png',
-    //         read: false
-    //     },
-    //     {
-    //         title: "les Miserables",
-    //         genre: "Historical Fiction",
-    //         author: 'Victor Hugo',
-    //         image_format: 'png',
-    //         read: false
-    //     },
-    //     {
-    //         title: "A Journey into the center of the Earth.",
-    //         genre: "Science Fiction",
-    //         author: 'Jules Verne',
-    //         image_format: 'png',
-    //         read: false
-    //     },
-    //     {
-    //         title: "The Dark World",
-    //         genre: "Fantasy",
-    //         author: 'Henry Kuttner',
-    //         image_format: 'png',
-    //         read: false
-    //     },
-    //     {
-    //         title: "The Time Machine",
-    //         genre: "Science Fiction",
-    //         author: 'H. G. Wells',
-    //         image_format: 'png',
-    //         read: false
-    //     },
-    //     {
-    //         title: "The Wind in the Willows",
-    //         genre: "Fantasy",
-    //         author: 'Kenneth Grahame',
-    //         image_format: 'jpg',
-    //         read: false
-    //     },
-    //     {
-    //         title: "Life on the Mississippi",
-    //         genre: "History",
-    //         author: 'Kenneth Grahame',
-    //         image_format: 'png',
-    //         read: false
-    //     },
-    //     {
-    //         title: "Life on the Mississippi",
-    //         genre: "History",
-    //         author: 'Kenneth Grahame',
-    //         image_format: 'png',
-    //         read: false
-    //     }
-    // ];
     bookRouter.route('/').get((req, res) => {
 
         // eslint-disable-next-line no-undef
@@ -105,7 +47,7 @@ function router(nav) {
 
                 client = await MongoClient.connect(url);
                 const db = client.db(dbName);
-                const book = await db.collection('books').find({ id });
+                const book = await db.collection('books').findOne({ _id: new ObjectID(id) });
                 // eslint-disable-next-line no-undef
                 res.render('single_book',
                     {
